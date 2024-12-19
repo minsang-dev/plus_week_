a5df3948eadd5058826a72c49cb4f89d695d86e4
Minsang <minsang1233@gmail.com>
2024-12-19 14:07:46 +0900

    Refactor: default 메소드

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    default User findByIdOrElseThrow(Long id){
+        return findById(id).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+    }
 }
